dafc9df5c1017c7aa21b20c3d725809fde3d64cc
Berkay Uygur <50241715+uygurberkay@users.noreply.github.com>
2023-08-14 15:26:05 +0300

    Add project files.

---
+++ METAYILDIZ/Controllers/HomeController.cs
@@ -0,0 +1,113 @@
+﻿using METAYILDIZ.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace METAYILDIZ.Controllers
+{
+	public class HomeController : Controller
+	{
+		
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Contact()
+		{
+			return View();
+		}
+
+        public IActionResult Founder()
+        {
+            return View();
+        }
+
+        public IActionResult AboutCompany()
+        {
+            return View();
+        }
+
+		public IActionResult YapayZeka()
+		{
+			return View();
+		}
+
+        public IActionResult VirtualReality()
+        {
+            return View();
+        }
+        public IActionResult EducationSimu()
+        {
+            return View();
+        }
+
+        public IActionResult WebDevelopment()
+        {
+            return View();
+        }
+
+        public IActionResult GameDevelopment()
+        {
+            return View();
+        }
+        
+        public IActionResult Vision()
+        {
+            return View();
+        }
+
+        public IActionResult Directors()
+        {
+            return View();
+        }
+        public IActionResult Corps()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult MSSavunmaDrone()
+        {
+            return View();
+        }
+        public IActionResult MSTarım()
+        {
+            return View();
+        }
+
+        public IActionResult MSGözetleme()
+        {
+            return View();
+        }
+
+        public IActionResult MSPuhu()
+        {
+            return View();
+        }
+
+        public IActionResult MSGölge()
+        {
+            return View();
+        }
+        public IActionResult MSGökcil()
+        {
+            return View();
+        }
+        public IActionResult MSVTOL()
+        {
+            return View();
+        }
+        public IActionResult MSIha()
+        {
+            return View();
+        }
+        public IActionResult MSRobot()
+        {
+            return View();
+        }
+    }
+}
